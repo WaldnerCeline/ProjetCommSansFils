a84aa07073b3b87ad6fb7cdee578ca88bbcfe01e
Axel Coutisson <50929677+AxelCoutisson@users.noreply.github.com>
2020-12-14 17:31:07 +0100

    upload

--- tutoriel/mafenetre.cpp
+++ tutoriel/mafenetre.cpp
@@ -8,6 +8,7 @@
 //========================================//
 #include "mafenetre.h"
 #include "ui_mafenetre.h"
+#include <cstring>
 //========================================//
 
 
@@ -84,15 +85,22 @@ void MaFenetre::on_Quitter_clicked()
 
 void MaFenetre::on_Acquisition_clicked()
 {
+    unsigned char data[240];
+    int status = 0;
     qDebug() << "[Acquisition]: Lancement de la fonction";
-    qDebug() << "[Acquisition]: affichage de la data avant appelle de la fonction:" << key_ff;
+    qDebug() << "[Acquisition]: affichage de la data avant appel de la fonction:" << key_ff;
     status = LEDBuzzer(&MonLecteur, LED_YELLOW_ON);
     if(status !=MI_OK)
     {
         qDebug() << "[Acquisition]: ERREUR affichage de la led jaune";
     }
-    key_index = 0;
-    status = Mf_Classic_LoadKey(&MonLecteur, TRUE , key_ff, key_index);//Auth_KeyA = true
+
+    status = Mf_Classic_LoadKey(&MonLecteur, Auth_KeyA , key_ff, 2);
+    if (status != MI_OK){
+        qDebug() << "Load Key [FAILED]\n";
+        return;
+    }
+    status = Mf_Classic_LoadKey(&MonLecteur, Auth_KeyB , key_ff, 2);
     if (status != MI_OK){
         qDebug() << "Load Key [FAILED]\n";
         return;
@@ -100,17 +108,9 @@ void MaFenetre::on_Acquisition_clicked()
     // RF field ON
     RF_Power_Control(&MonLecteur, TRUE, 0);
 
-    status = ISO14443_3_A_PollCard(&MonLecteur, atq, sak, uid, &uid_len);
-    if (status != MI_OK){
-        qDebug() << "No available tag in RF field\n";
-        return;
-    }
-
-    qDebug() <<"Tag found: UID=";
-    for (int i = 0; i < uid_len; i++)
-        qDebug() << uid[i];
-    qDebug() <<" ATQ= " << atq[1] << ' ' << atq[0] <<" SAK=" <<sak[0];
-
+    status = Mf_Classic_Read_Block(&MonLecteur,TRUE,9,data, Auth_KeyA, 2);
+    for (int i = 0; i < 240; i++){
+        //print data bruh
 }
 //========================================//
 
